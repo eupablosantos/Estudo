4f14202c92fefca40e7946ddabd4ea683ecb7199
Pablo Santos <111190417+eupablosantos@users.noreply.github.com>
2022-08-13 17:35:48 -0300

    Add files via upload
    Neste exercício foi criado um mini questiońario onde existem 2 pessoas, sendo 1 mais velha. Logo, é pedido para inserir o nome e idade de cada uma das pessoas, e no final é apresentado a diferença de idade entre elas. Este arquivo vai junto com o arquivo "index2.html" para que seja visto no navegador.

---
+++ exercicio2.js
@@ -0,0 +1,12 @@
+let nomev = prompt("Digite o nome da pessoa mais velha")
+let idadev = prompt("Qual a sua idade?")
+
+let nomen = prompt("Digite o nome da pessoa mais nova")
+let idaden = prompt("Qual a sua idade?")
+
+let diferencaidade = idadev - idaden
+
+alert(
+    "Pessoa mais velha: " + nomev + "\nIdade: " + idadev + 
+    "\n\nPessoa mais nova: " + nomen + "\nIdade: " + idaden +
+    "\n\nDiferença de idade: " + diferencaidade)
