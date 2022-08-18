a3f87751f231415e221b5703268fefbc101c4780
Pablo Santos <111190417+eupablosantos@users.noreply.github.com>
2022-08-18 14:56:33 -0300

    Add files via upload
    Aqui vai um exemplo de como declarar parametros dentro de uma função para retornar um novo valor.

---
+++ parametrofuncao.js
@@ -0,0 +1,10 @@
+function speed(velocidade, aceleracao) { //aqui o speed vai receber o valor de velocidade + aceleracao, que serão declarados no final da função
+    let novavelocidade = velocidade + aceleracao //declarando o valor de "novavelocidade"
+    console.log("Nova velocidade: " + novavelocidade) 
+}
+
+speed(60, 10) //aqui estou declarando o valor de velocidade / aceleracao respectivamente
+
+speed(40, 20) //aqui estou declarando o valor de velocidade / aceleracao respectivamente
+
+speed(40, 50) //aqui estou declarando o valor de velocidade / aceleracao respectivamente
