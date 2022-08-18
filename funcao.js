e2146df06c13bcf51d350a89b146accce6a09c74
Pablo Santos <111190417+eupablosantos@users.noreply.github.com>
2022-08-18 14:36:07 -0300

    Add files via upload
    Começo dos estudos sobre "function". Iniciando uma função.

---
+++ funcao.js
@@ -0,0 +1,7 @@
+//função é um bloco de código nomeado que podemos declarar para utilizar mais a frente no código
+
+function piloto() { //criando a função
+    alert("Bom dia, Piloto!")
+}
+
+piloto() //para ser executada a função precisa ser chamada no final do código
